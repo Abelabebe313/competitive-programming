e11a2c054d3d150cab229e59f23f64a06ea829fd
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-01-10 19:28:56 +0300

    Time: 517 ms (89.74%), Space: 20.5 MB (78.28%) - LeetHub

---
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
@@ -0,0 +1,42 @@
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        output = []
+        currSum = 0
+        for num in nums:
+            if not num % 2:
+                currSum += num
+        for querie in queries:
+            if querie[0] % 2 == 0 and nums[querie[1]] %2 == 0:
+                currSum += querie[0]
+                nums[querie[1]] += querie[0]
+                output.append(currSum)
+            elif querie[0] % 2 != 0 and nums[querie[1]] %2 != 0:
+                nums[querie[1]] += querie[0]
+                currSum += nums[querie[1]]
+                output.append(currSum)
+            elif querie[0] % 2 == 0 and nums[querie[1]] %2 != 0:
+                nums[querie[1]] += querie[0]
+                output.append(currSum)
+            elif querie[0] % 2 != 0 and nums[querie[1]] %2 == 0:
+                currSum -= nums[querie[1]]
+                nums[querie[1]] += querie[0]
+                output.append(currSum)
+        return output
+                
+        
+        
+        
+            
+#         for i in range(len(queries)):
+#             vali = queries[i][0]
+#             indexi = queries[i][1]
+            
+#             nums[indexi] = nums[indexi] + vali
+#             tempSum = 0
+#             for j in nums:
+                
+#                 if j%2 == 0:
+#                     tempSum += j
+#             output.append(tempSum)
+#         return output
+                    
