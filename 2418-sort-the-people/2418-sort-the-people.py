a9cbf25ff0f04e8179b96613472a1102f6940a67
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-01-17 18:43:32 +0300

    Time: 583 ms (10.59%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,26 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        swap = 0
-        sort = False
-        nameDict = {}
-        while not sort:
-            sort = True
-            for i in range(len(heights)-1):
-                if heights[i] < heights[i+1]:
-                    sort = False
-                    heights[i],heights[i+1] = heights[i+1],heights[i]
-                    names[i],names[i+1] = names[i+1],names[i]
-                    swap +=1
+        for i in range(len(heights)):
+            minn = i
+            for j in range(i+1,len(heights)):
+                if heights[minn] < heights[j]:
+                    minn = j
+            if minn != i:
+                heights[i],heights[minn] = heights[minn],heights[i]
+                names[i],names[minn] = names[minn],names[i]
+        return names
+                
+                
+                
+        # swap = 0
+        # sort = False
+        # nameDict = {}
+        # while not sort:
+        #     sort = True
+        #     for i in range(len(heights)-1):
+        #         if heights[i] < heights[i+1]:
+        #             sort = False
+        #             heights[i],heights[i+1] = heights[i+1],heights[i]
+        #             names[i],names[i+1] = names[i+1],names[i]
+        #             swap +=1
         return names
