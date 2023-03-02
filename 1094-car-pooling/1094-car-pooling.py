5094661c62397ba3be45bf7f48c1d081c2b40503
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-03-02 20:52:33 +0300

    Time: 75 ms (68.27%), Space: 14.3 MB (79.75%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,18 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        lst = []
-        current = 0
-        for ppl, strt,end in trips:
-            lst.append((strt,ppl,1))
-            lst.append((end,ppl,0))
+        trips.sort(key=lambda x:x[2],reverse=True)
+        maximum = trips[0][2]
+        lineSweep = [0] * (maximum + 1)
         
-        lst.sort(key = lambda x: (x[0],x[2]))
-        
-        for time,people,pickup in lst:
-            if pickup:
-                current += people
-            else:
-                current -= people
-            if current > capacity:
+        for passengers, start, end in trips:
+            lineSweep[start] += passengers
+            lineSweep[end] -= passengers
+        total = 0
+        for num in lineSweep:
+            total += num
+            if total > capacity:
                 return False
         return True
