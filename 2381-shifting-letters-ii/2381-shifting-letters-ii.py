02710053abd670865d649daef3dd2e4eb4556e43
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-03-02 22:05:28 +0300

    Time: 1344 ms (80.85%), Space: 38.6 MB (99.39%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        lineSweep = [0] * (len(s)+1)
+        
+        for start, end, direction in shifts: 
+            if direction:
+                lineSweep[start] += 1
+                lineSweep[end+1] -= 1
+            else:
+                lineSweep[start] -= 1
+                lineSweep[end+1] += 1
+        print(lineSweep)        
+        total = 0
+        result = ''
+        for i,char in enumerate(s):
+            total += lineSweep[i]
+            result += chr((((ord(char) + total) - 97) % 26) + 97) 
+            
+                
+        return result
+        
+        
