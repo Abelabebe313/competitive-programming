3881007a2f080a6e5b016287ce250b897c70f2fa
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-03-29 18:33:17 +0300

    Time: 216 ms (8.47%), Space: 20.7 MB (66.00%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,15 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        def recurtion(num):
-            if num > n:
-                return
-            
-            binar = bin(num)
-            ans.append(binar.count('1'))
-            recurtion(num+1)
-            
-        recurtion(0)
+        ans = [0] * (n+1)
+        for i in range(n+1):
+            index = i
+            while i:
+                if i & 1:
+                    ans[index] += 1
+                    
+                i >>= 1
+                
         return ans
         
         
