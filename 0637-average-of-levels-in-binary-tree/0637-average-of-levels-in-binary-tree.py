a28f8a91654063cd65371a91a55c8e0776e938ae
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-04-26 18:18:56 +0300

    Time: 63 ms (7.87%), Space: 18.8 MB (7.70%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -7,24 +7,23 @@
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         avg = []
-        def BFS(root):
-            nonlocal avg
-            queue = deque()
-            queue.append(root)
-            
-            while queue:
-                count = len(queue)
-                summ = 0
-                for i in range(len(queue)):
-                    node = queue.popleft()
-                    summ += node.val
-                    if node.left:
-                        queue.append(node.left)
-                    if node.right:
-                        queue.append(node.right)
+        queue = deque()
+        queue.append(root)
 
-                avg.append(summ/count)
-        BFS(root)
+        while queue:
+            count = len(queue)
+            summ = 0
+            temp = []
+            for node in queue:
+                summ += node.val
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+
+            avg.append(summ/count)
+            queue = temp
+        
         return avg
         
             
