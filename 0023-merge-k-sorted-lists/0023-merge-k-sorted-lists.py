a56dac977db03d6f3f24b0b8d4b105881b83a3a8
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-05-07 13:43:10 +0300

    Time: 114 ms (41.97%), Space: 20.2 MB (7.22%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,37 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        def merge(l1,l2):
-            dummy = ListNode()
-            curr = dummy
+        heap = []
+        ans = []
+        
+        for i in range(len(lists)):
+            node = lists[i]
+            if lists[i]:
+                heappush(heap,(node.val,i))
+                lists[i] = lists[i].next
+                
+        dummy = ListNode()
+        cur = dummy
+        
+        while heap:
+            node, idx = heappop(heap)
+            cur.next = ListNode(node)
+            cur = cur.next
             
-            while l1 and l2:
-                if l1.val > l2.val:
-                    curr.next = l2
-                    l2 = l2.next
-                else:
-                    curr.next = l1
-                    l1 = l1.next
-                curr = curr.next
-            if l1:
-                curr.next = l1
-            
-            if l2:
-                curr.next = l2
-            return dummy.next
-        
-        if not lists and len(lists)==0:
-            return None
-        while len(lists) > 1:
-            newList = []
-            
-            for i in range(0,len(lists),2):
-                lis1 = lists[i]
-                lis2 = lists[i+1] if (i+1) < len(lists) else None
-                newList.append(merge(lis1,lis2))
-        
-            lists = newList
-        return lists[0]
-        
-        
+            if lists[idx]:
+                heappush(heap,(lists[idx].val,idx))
+                lists[idx] = lists[idx].next
+        return dummy.next
             
