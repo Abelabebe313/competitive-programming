1a62a567804685b392db5d137699766c8a0d73dd
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-05-09 18:38:34 +0300

    Time: 108 ms (37.50%), Space: 17.8 MB (17.71%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        incomming = [0] * numCourses
+        ans = []
+        queue = deque([])
+        
+        for a,b in prerequisites:
+            graph[b].append(a)
+            incomming[a] += 1
+            
+        for idx,count in enumerate(incomming):
+            if count == 0:
+                queue.append(idx)
+        while queue:
+            cur_node = queue.popleft()
+            ans.append(cur_node)
+            for neighbour in graph[cur_node]:
+                incomming[neighbour] -= 1
+                if incomming[neighbour]==0:
+                    queue.append(neighbour)
+        if len(ans)!=numCourses:
+            return []
+        return ans
