e6cca3a99c0f45fe622391d42514a8c4a9cc2b1d
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-05-10 20:01:10 +0300

    Time: 645 ms (88.58%), Space: 23.5 MB (26.28%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,26 +1,27 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        graph_hash = defaultdict(list)
-        incomming = [0] * len(graph)
-        qu = deque([])
-        terminal = []
-        
-        for idx in range(len(graph)):
-            for num in graph[idx]:
-                graph_hash[num].append(idx)
-            incomming[idx] = len(graph[idx])
-            
-        for i in range(len(incomming)):
-            if incomming[i] == 0:
-                qu.append(i)
-        
-        while qu:
-            cur_node = qu.popleft()
-            terminal.append(cur_node)
-            for nbr in graph_hash[cur_node]:
-                incomming[nbr] -= 1
-                if incomming[nbr]==0:
-                    qu.append(nbr)
-        terminal.sort()
-        return terminal
-        
+    
+        color = [0] * len(graph)
+        # white = 0
+        # gray = 1
+        # black = 2
+        ans = []
+        def DFS(node):
+            if color[node]==1:
+                return False
+            if color[node]==2:
+                return True
+           
+            color[node] = 1
+            for child in graph[node]:
+                if not DFS(child):
+                    return False
+            color[node] = 2
+            ans.append(node)
+            return True
+                
+        for i in range(len(graph)):
+            if color[i] != 2:
+                DFS(i)
+        ans.sort()
+        return ans
