7daa3438477cca413c3553203ad1eabf76eb2f3f
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-09-24 10:20:11 +0300

    Time: 628 ms (5.88%), Space: 16.1 MB (84.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,17 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        # with memory
-        memo = {}
-        def fibonaci(num):
-            if num == 0:
+        memo = defaultdict(int)
+        
+        def fib(i):
+            if i == 0:
                 return 0
-            if num == 1:
+            if i == 1:
                 return 1
-            if num in memo:
-                return memo[num]
-            memo[num] = fibonaci(num-1) + fibonaci(num-2)
-            return memo[num]
-        
-        return fibonaci(n)
-            
-        
+            memo[i] = fib(i-1) + fib(i-2)
+            return memo[i]
+        return fib(n)
