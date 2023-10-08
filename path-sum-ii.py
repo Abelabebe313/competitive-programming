500446b85f95e1fce7bc760dba849f66f38f26a2
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2023-10-08 18:02:55 +0300

    added path-sum-ii.python

---
+++ path-sum-ii.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        ans = []
+        def traverse(root,pathSum,path):
+            # nonlocal ans
+            if not root:
+                return
+            pathSum += root.val
+            path.append(root.val)
+            if not root.left and not root.right:
+                if targetSum == pathSum:
+                    ans.append(path[::])
+                    return path.pop()
+            
+            traverse(root.left,pathSum,path)
+            traverse(root.right,pathSum,path)
+            path.pop()
+        traverse(root,0,[])
+        return ans
